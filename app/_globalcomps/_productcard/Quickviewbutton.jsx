5e330089f2d50dda69829df993b2ddec50a268a9
parvesh <polidahiya830@gmail.com>
2025-07-19 22:54:15 +0530

    product page button fix

--- app/_globalcomps/_productcard/Quickviewbutton.jsx
+++ app/_globalcomps/_productcard/Quickviewbutton.jsx
@@ -6,7 +6,7 @@ import { AppContextfn } from "@/app/Context";
 function Quickviewbutton({ product }) {
   const { setquickview } = AppContextfn();
   return (
-    <div className="absolute top-0 lg:bottom-0 right-0  w-fit lg:w-full p-2 lg:hidden lg:group-hover:block z-10">
+    <div className="absolute top-0 lg:top-auto lg:bottom-0 right-0  w-fit lg:w-full p-2 lg:hidden lg:group-hover:block z-10">
       <button
         className="w-full p-3 lg:p-5 bg-white text-theme rounded-full lg:rounded-none opacity-50 lg:opacity-100 lg:hover:bg-theme lg:hover:text-white duration-300"
         onClick={() => setquickview({ show: true, data: product })}
